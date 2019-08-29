7a569c3ac1a437f5b9e2d91602424323e6e502bf
jishin <jishin86@gmail.com>
2019-08-29 20:37:40 +0900

    backup of ROOT 5.32 doxygen

---
+++ docs/v532/RConfigOptions.h
@@ -0,0 +1,10 @@
+#ifndef ROOT_RConfigOptions
+#define ROOT_RConfigOptions
+
+#define R__CONFIGUREOPTIONS   "RFIO=/home/rdm/castor-2.1.9/usr CASTOR=/home/rdm/castor-2.1.9/usr PYTHIA6=/home/rdm/pythia6 PYTHIA8=/home/rdm/pythia8145 FFTW3=/home/rdm/fftw-3.1.1 DCACHE=/home/rdm/dcache_client/1.8.0p1/slc4_amd64_gcc34/dcap CHIRP=/home/rdm/chirp ALIEN=/opt/alien/i386 MONALISA=/home/rdm/ApMon_cpp-2.2.6 SRP=/home/rdm/src/srp-2.1.2-beta2 TABLE=yes --build=opt --all"
+#define R__CONFIGUREFEATURES  "asimage astiff bonjour builtin_afterimage builtin_lzma castor cintex dcache exceptions explicitlink fftw3 fitsio gviz gdml genvector krb5 ldap mathmore memstat minuit2 monalisa mysql opengl pythia6 pythia8 python qt qtgsi reflex roofit ruby rfio shadowpw shared ssl table tmva unuran x11 xft xml thread"
+
+#endif
+
+#define R__RUBY_MAJOR 1
+#define R__RUBY_MINOR 8
